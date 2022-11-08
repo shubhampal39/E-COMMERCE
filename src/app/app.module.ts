39a8e3eee29d3bd35e771889ddc81cfd99cb18ba
shubhampal39 <40192314+shubhampal39@users.noreply.github.com>
2022-11-08 08:01:16 +0530

    5 Added seller form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, HelloComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    SellerAuthComponent,
+    HomeComponent,
+    HeaderComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
